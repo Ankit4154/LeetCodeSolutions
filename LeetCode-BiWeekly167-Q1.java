0a57b4fc2f51c592003d389f6f1762627a0106f7
Ankit Singh <ankitsingh4154@gmail.com>
2025-10-11 21:44:18 +0530

    Update LeetCode-BiWeekly167-Q1.java

--- LeetCode-BiWeekly167-Q1.java
+++ LeetCode-BiWeekly167-Q1.java
@@ -9,8 +9,11 @@ class Solution {
         // then right++ and check if left score == right score
         // left <= right
         // if we find left score == right score 
-        // return true
-        // else if loop ends and none found return false
+        // then check if we have parsed the entire string or not
+		// if we have parsed all characters of string then return true ( case : cba, adcb)
+		// else if still some characters of string are remaining to be parsed
+		// increment left, decrement right and continue with the loop (case : aaaa)
+        // else if loop ends and none found (case : jhj, bace) return false
         int left = 0, right = s.length()-1, sumLeft = 0, sumRight = 0;
         sumLeft += s.charAt(left) - 'a' + 1;
 		sumRight += s.charAt(right) - 'a' + 1;
@@ -32,3 +35,4 @@ class Solution {
     }
 
 }
+
