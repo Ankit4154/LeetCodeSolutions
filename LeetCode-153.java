26df6ebd89ec85fb3ae7d64243ec67a17750d6a6
Ankit Singh <ankitsingh4154@gmail.com>
2025-11-08 13:42:09 +0530

    Update LeetCode-153.java

--- LeetCode-153.java
+++ LeetCode-153.java
@@ -1,25 +1,58 @@
 // 153. Find Minimum in Rotated Sorted Array
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
 public class Solution {
     public int findMin(int[] nums) {
-        int l = 0;
-        int r = nums.length - 1;
-        int res = nums[0];
-
-        while (l <= r) {
-            if (nums[l] < nums[r]) {
-                res = Math.min(res, nums[l]);
+        int low = 0, high = nums.length -1;
+		int ans = Integer.MAX_VALUE;
+        // in given array, when you point to mid, there will always be
+        // one part of the array sorted and other part will be not sorted.
+        // your rotation point will always lie in the part which is not sorted.
+        // store the current min value from sorted part and then pivot to the non-sorted part.
+		while(low <= high){
+            // if value at low is already less than value at high
+            // it means the entire array is already sorted
+            // and the min value is present at low
+            if (nums[low] < nums[high]) {
+                ans = Math.min(ans, nums[low]);
                 break;
             }
-
-            int m = l + (r - l) / 2;
-            res = Math.min(res, nums[m]);
-            if (nums[m] >= nums[l]) {
-                l = m + 1;
-            } else {
-                r = m - 1;
-            }
-        }
-        return res;
+			int mid = low + (high-low)/2;
+			// if left half is sorted
+			if(nums[low] <= nums[mid]){
+				ans = Math.min(ans, nums[low]);
+				low = mid + 1; // pivot to other un-sorted half
+			}else{ // if right half is sorted
+				ans = Math.min(ans, nums[mid]);
+				high = mid - 1; // pivot to other un-sorted half
+			}
+		}
+        return ans;
     }
 }
+
+
+// public class Solution {
+//     public int findMin(int[] nums) {
+//         int l = 0;
+//         int r = nums.length - 1;
+//         int res = nums[0];
+
+//         while (l <= r) {
+//             if (nums[l] < nums[r]) {
+//                 res = Math.min(res, nums[l]);
+//                 break;
+//             }
+
+//             int m = l + (r - l) / 2;
+//             res = Math.min(res, nums[m]);
+//             if (nums[m] >= nums[l]) {
+//                 l = m + 1;
+//             } else {
+//                 r = m - 1;
+//             }
+//         }
+//         return res;
+//     }
+
+// }
