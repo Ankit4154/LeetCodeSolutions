d509efbd70473204021e22bee4912a6aafbc8fd6
Ankit Singh <ankitsingh4154@gmail.com>
2025-12-18 20:56:36 +0530

    136. Single Number

---
+++ LeetCode-136.java
@@ -0,0 +1,13 @@
+// 136. Single Number
+// https://leetcode.com/problems/single-number/
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+
+        for(int num : nums){
+            result ^= num;
+        }
+
+        return result;
+    }
+}
