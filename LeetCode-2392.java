48aad553fb2d5f4de2ee2ad6a59aa2f508321a90
Ankit Singh <ankitsingh4154@gmail.com>
2026-01-17 23:51:19 +0530

    2392. Build a Matrix With Conditions

--- LeetCode-2392.java
+++ LeetCode-2392.java
@@ -1,5 +1,75 @@
 // 2392. Build a Matrix With Conditions
 // https://leetcode.com/problems/build-a-matrix-with-conditions/
+// optimize time using primitive data structures 
+// Tip : Can use same visited array for path check marking 2 in visited array for every path traversal
+class Solution {
+	
+    public int[][] buildMatrix(int k, int[][] rowedges, int[][] coledges){
+		List<Integer> rowList = topoSort(k, rowedges);
+		List<Integer> colList = topoSort(k, coledges);
+		if(rowList.isEmpty() || colList.isEmpty())
+			return new int[][]{};
+		Map<Integer, Integer> valToRow = new HashMap<>();
+		Map<Integer, Integer> valToCol = new HashMap<>();
+		for(int i=0;i<rowList.size();i++){
+			valToRow.put(rowList.get(i), i);
+		}
+		for(int i=0;i<colList.size();i++){
+			valToCol.put(colList.get(i), i);
+		}
+		int[][] out = new int[k][k];
+		for(int n=1;n<=k;n++){
+			int row =  valToRow.get(n);
+			int col =  valToCol.get(n);
+			out[row][col] = n;
+		}
+		return out;
+		
+    }
+	
+	public List<Integer> topoSort(int k, int[][] edges){
+		List<List<Integer>> graph = new ArrayList<>();
+		
+		for(int i=0;i<=k;i++){
+			graph.add(new ArrayList<>());
+		}
+		for(int i=0;i<edges.length;i++){
+			int u = edges[i][0];
+			int v = edges[i][1];
+			graph.get(u).add(v);
+		}
+		boolean[] visited = new boolean[k+1];
+        int[] path = new int[k+1];
+		List<Integer> list = new ArrayList<>();
+		for(int i=1;i<=k;i++){
+            if(!visited[i]){
+				if(!dfs(i, list, graph, visited, path)){
+					return new ArrayList<>();
+				}
+			}
+		}
+		Collections.reverse(list);
+		return list;
+	}
+	
+	public boolean dfs(int n, List<Integer> list, List<List<Integer>> graph, boolean[] visited, int[] path){
+		if(path[n] == 1)
+            return false;
+        if(visited[n])
+            return true;
+        visited[n] = true;
+        path[n] = 1;
+		for(int nei : graph.get(n)){
+			if(!dfs(nei, list, graph, visited, path))
+                return false;
+		}
+        path[n] = 0;
+		list.add(n);
+        return true;
+	}
+}
+
+
 public class Solution{
     private Set<Integer> visit;
     private Set<Integer> path;
@@ -69,4 +139,5 @@ public class Solution{
         order.add(src);
         return true;
     }
+
 }
