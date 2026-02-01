fb07b26aee92d4ef34a50d6e02ce23b5b40e988d
Ankit Singh <ankitsingh4154@gmail.com>
2026-02-01 13:30:02 +0530

    257. Binary Tree Paths

---
+++ LeetCode-257.java
@@ -0,0 +1,46 @@
+// 257. Binary Tree Paths
+// https://leetcode.com/problems/binary-tree-paths/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<>();
+        dfs(root, new StringBuilder(), result);
+        return result;
+    }
+
+    private void dfs(TreeNode node, StringBuilder path, List<String> result) {
+        if (node == null) return;
+
+        int length = path.length();
+
+        if (length > 0) {
+            path.append("->");
+        }
+        path.append(node.val);
+
+        // Leaf node
+        if (node.left == null && node.right == null) {
+            result.add(path.toString());
+        } else {
+            dfs(node.left, path, result);
+            dfs(node.right, path, result);
+        }
+
+        // Backtrack
+        path.setLength(length);
+    }
+}
