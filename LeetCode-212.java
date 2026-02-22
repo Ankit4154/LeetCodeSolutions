0d7e05423fcd166134363ef59040ee6d408f3615
Ankit Singh <ankitsingh4154@gmail.com>
2026-02-22 15:33:21 +0530

    212. Word Search II

--- LeetCode-212.java
+++ LeetCode-212.java
@@ -1,6 +1,171 @@
 // 212. Word Search II
 // https://leetcode.com/problems/word-search-ii/
 
+// optimized
+class TrieNode{
+    TrieNode[] children = new TrieNode[26];
+    boolean flag = false; // true means end of word
+	String word; // non-null means end of word
+}
+class Solution {
+	List<String> res = new ArrayList<>();
+	int[] dRow = new int[]{-1,1,0,0};
+	int[] dCol = new int[]{0,0,-1,1};
+	public List<String> findWords(char[][] board, String[] words) {
+		TrieNode root = buildTrie(words);
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                solve(i, j, board, root);                
+            }
+        }
+		
+		return res;
+    }
+	TrieNode buildTrie(String[] words){
+		TrieNode root = new TrieNode();
+		for(String word : words){
+			TrieNode node = root;
+			for(int i=0;i<word.length();i++){
+				char c = word.charAt(i);
+				if(node.children[c - 'a'] == null){
+					node.children[c - 'a'] = new TrieNode();
+				}
+				node = node.children[c - 'a'];
+			}
+			node.flag = true;
+			node.word = word;
+		}
+		return root;
+	}
+	
+	public void solve(int r, int c, char[][] board, TrieNode node){
+		char ch = board[r][c];
+		// if already visited or if character is not present in Trie
+		if(ch == '0' || node.children[ch - 'a'] == null)
+			return;
+		
+		node = node.children[ch - 'a'];
+		if(node.word != null){
+			res.add(node.word);
+			node.word = null;  // prevent duplicates
+		}
+        
+        board[r][c] = '0'; // mark visited
+		
+		for(int d=0;d<4;d++){
+			int row = r + dRow[d];
+			int col = c + dCol[d];
+			if(row < 0 || row >= board.length
+				|| col < 0 || col >= board[0].length
+                || board[row][col] == '0'
+			)
+				continue;
+			solve(row, col, board, node);
+		}
+		// backtrack
+        board[r][c] = ch;
+	}
+}
+
+
+// init
+class TrieNode{
+    Node[] links = new Node[26];
+    boolean flag = false;
+    public Node(){}
+    public boolean containsKey(char c){
+        return links[c - 'a'] != null;
+    }
+    public Node get(char c){
+        return links[c - 'a'];
+    }
+    public void put(char c, Node n){
+        links[c - 'a'] = n;
+    }
+	public void setEnd(){
+		this.flag = true;
+	}
+	public boolean getEnd(){
+		return this.flag;
+	}
+}
+class Trie{
+	
+	Node root;
+	Trie(){
+		root = new Node();
+	}
+	void insert(String word){
+		Node node = root;
+		for(int i=0;i<word.length();i++){
+			char c = word.charAt(i);
+			if(!node.containsKey(c)){
+				node.put(c, new Node());
+			}
+			node = node.get(c);
+		}
+		node.setEnd();
+	}
+	boolean searchWord(String word){
+		Node node = root;
+		for(int i=0;i<word.length();i++){
+			char c = word.charAt(i);
+			if(!node.containsKey(c)){
+				return false;
+			}
+			node = node.get(c);
+		}
+		return true;
+	}
+	
+}
+
+class Solution {
+	List<String> res = new ArrayList<>();
+	Trie trie = new Trie();
+	public List<String> findWords(char[][] board, String[] words) {
+		Set<String> wordsSet = new HashSet<>();
+		for(String word : words){
+			trie.insert(word);
+			wordsSet.add(word);
+		}
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                solve(i, j, board, wordsSet, board[i][j]+"");                
+            }
+        }
+		
+		return res;
+    }
+	public void solve(int r, int c, char[][] board, Set<String> words, String word){
+		if(words.contains(word)){
+			res.add(new String(word));
+            words.remove(word);
+		}
+        char temp = board[r][c];
+        board[r][c] = '0'; // mark visited
+		int[] dRow = new int[]{-1,1,0,0};
+		int[] dCol = new int[]{0,0,-1,1};
+		for(int d=0;d<4;d++){
+			int row = r + dRow[d];
+			int col = c + dCol[d];
+			if(row < 0 || row >= board.length
+				|| col < 0 || col >= board[0].length
+                || board[row][col] == '0'
+			)
+				continue;
+            word += board[row][col];
+			if(trie.searchWord(word)){
+				solve(row, col, board, words, word);
+			}
+            // backtrack
+            word = word.substring(0,word.length()-1);
+		}
+        board[r][c] = temp;
+	}
+}
+
+
 class Solution {
 	List<String> res = new ArrayList<>();
 	int[] dRow = new int[]{-1,1,0,0};
@@ -183,4 +348,5 @@ class Solution {
 		board[r][c] = orig;
 		return false;
 	}
+
 }
