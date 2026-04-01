53599ccee00f8ef7b6036ca971b94ae050ef45f7
Ankit Singh <ankitsingh4154@gmail.com>
2026-04-01 23:18:25 +0530

    152. Maximum Product Subarray

---
+++ LeetCode-152.java
@@ -0,0 +1,18 @@
+// 152. Maximum Product Subarray
+// https://leetcode.com/problems/maximum-product-subarray/
+public class Solution {
+    public int maxProduct(int[] nums){
+        int res = nums[0];
+
+        for(int i = 0; i < nums.length; i++){
+            int cur = nums[i];
+            res = Math.max(res, cur);
+            for(int j = i + 1; j < nums.length; j++){
+                cur *= nums[j];
+                res = Math.max(res, cur);
+            }
+        }
+
+        return res;
+    }
+}
