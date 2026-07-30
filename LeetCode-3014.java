7f4df304ba3a258f005d4b4be8d461f0e744cceb
Ankit Singh <ankitsingh4154@gmail.com>
2026-07-30 18:38:30 +0530

    3014. Minimum Number of Pushes to Type Word I

---
+++ LeetCode-3014.java
@@ -0,0 +1,41 @@
+// 3014. Minimum Number of Pushes to Type Word I
+// https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i/
+// optim, removed HashMap
+class Solution {
+    public int minimumPushes(String word) {
+        int[] alpha = new int[26];
+		int count = 1, sum = 0;
+		for(int i=0;i<word.length();i++){
+			char letter = word.charAt(i);
+			int pos = (int)letter - 'a';
+			if(alpha[pos] == 0){
+				alpha[pos] = count;
+				sum += count;
+			}else{
+				sum += alpha[pos];
+			}
+			if((i+1) % 8 == 0)
+				count++;
+		}
+		return sum;
+    }
+}
+
+class Solution {
+    public int minimumPushes(String word) {
+        Map<Character, Integer> map = new HashMap<>(26);
+		int count = 1, sum = 0;
+		for(int i=0;i<word.length();i++){
+			char letter = word.charAt(i);
+			if(!map.containsKey(letter)){
+				map.put(letter, count);
+				sum += count;
+			}else{
+				sum += map.get(letter);
+			}
+			if((i+1) % 8 == 0)
+				count++;
+		}
+		return sum;
+    }
+}
