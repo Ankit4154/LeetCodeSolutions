3cb07bfb03c5e07d8d3dd50f5d89c751e284df2c
Ankit Singh <ankitsingh4154@gmail.com>
2026-08-23 22:12:20 +0530

    1927. Sum Game

---
+++ LeetCode-1927.java
@@ -0,0 +1,34 @@
+// 1927. Sum Game
+// https://leetcode.com/problems/sum-game
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int mid = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < mid; i++) {
+            if (num.charAt(i) == '?') {
+                leftQ++;
+            } else {
+                leftSum += num.charAt(i) - '0';
+            }
+        }
+
+        for (int i = mid; i < n; i++) {
+            if (num.charAt(i) == '?') {
+                rightQ++;
+            } else {
+                rightSum += num.charAt(i) - '0';
+            }
+        }
+
+        int diff = leftSum - rightSum;
+        int qDiff = leftQ - rightQ;
+
+        return diff * 2 != -9 * qDiff;
+    }
+}
