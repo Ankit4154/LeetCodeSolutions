760ec93b8ed104df2fb586b300586bfc25c6f794
Ankit Singh <ankitsingh4154@gmail.com>
2026-09-23 21:15:36 +0530

    796. Rotate String

---
+++ LeetCode-796.java
@@ -0,0 +1,10 @@
+// 796. Rotate String
+// https://leetcode.com/problems/rotate-string/
+class Solution {
+    public boolean rotateString(String s, String goal) {
+		 if(s.length() != goal.length())
+            return false;
+
+        return (s + s).contains(goal);
+    }
+}
